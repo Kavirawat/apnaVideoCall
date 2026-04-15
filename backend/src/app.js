abfe0050b8ae7c21301581a052ef878b63d9e78e
kavi rawat <kavirrawat896@gmail.com>
2026-04-15 12:06:03 +0530

    add some files

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,14 @@ import mongoose from "mongoose";
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 
 /* =================== Create Server ========================== */
 const server = createServer(app);
